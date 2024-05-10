17cc61f7db991e384c8864912a9d29e24f873678
kalam-321 <shaikabdulkalam9390@gmail.com>
2024-05-10 23:44:57 +0530

    Third

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,49 @@
 import RestaurantCard from "./RestaurantCard";
-
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = ()=>{  
     // let listOfRestaurants = resList;
 
     // local state variable using useState()
     // let or const both work in this case. We can even change the state of const variable.
-    const [listOfRestaurants,setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants,setListOfRestaurants] = useState([]);
+    const [filterdRestaurants,setFilteredRestaurants] = useState([]);
+    const [searchText,setSearchText] = useState("");
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async ()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=15.793212&lng=78.0441517&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    console.log("Body rendered");
+    // Conditional Rendering
+    if(listOfRestaurants.length===0)
+    {
+        return <Shimmer/>;
+    }
     return (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText}
+                    onChange={(e)=>{
+                        setSearchText(e.target.value);
+                    }}/> 
+                    <button onClick={()=>{
+                        console.log(searchText);
+                        const filteredRestaurant = listOfRestaurants.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                        setFilteredRestaurants(filteredRestaurant);
+                    }}>Search</button>
+                </div>
                 <button className="filter-btn" onClick={()=>{
                     const filteredList = listOfRestaurants.filter((res)=>res.info.avgRating>4);
-                    setListOfRestaurants(filteredList);
+                    setFilteredRestaurants(filteredList);
                     console.log(listOfRestaurants);
                 }}>
                     Top rated Restaurants
@@ -22,7 +51,7 @@ const Body = ()=>{
             </div>
             <div className="res-container">
                 {
-                    listOfRestaurants.map(restaurant=>(
+                    filterdRestaurants.map(restaurant=>(
                         <RestaurantCard key = {restaurant.info.id} resData={restaurant}/>
                     ))
                 }             
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = ()=>{
+    // let btn = "Login";
+    const [btn,setBtn] = useState("Login");
     return (
         <div className="header">
             <div className="logo-container">
@@ -12,6 +15,9 @@ const Header = ()=>{
                     <li>About</li>
                     <li>Contact us</li>
                     <li>Cart</li>
+                    <button className="login-btn" onClick={()=>{
+                       btn==="Login"? setBtn("Logout") : setBtn("Login");
+                    }}>{btn}</button>
                 </ul>
             </div>
         </div>
